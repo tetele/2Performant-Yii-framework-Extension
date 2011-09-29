94af5ddd3ab68c9462e9779f7718c76bfdfa1c96
Tudor Sandu <tm.sandu@gmail.com>
2011-09-29 19:18:12 +0300

    Removed hard-coded API component name. When using a different API component identifier than tpapi, OAuth apps failed to function. Now they work.

--- TPAPI/CTPAppController.php
+++ TPAPI/CTPAppController.php
@@ -6,25 +6,27 @@ class CTPAppController extends Controller
 		return $this->createAbsoluteUrl('callback');
 	}
 	
-	/**
-	 * This is the default 'index' action that is invoked
-	 * when an action is not explicitly requested by users.
-	 */
 	public function actionInit() {
-//		Yii::trace(Yii::getPathOfAlias('ext.TPAPI.2Performant.PEAR.HTTP.OAuth.Consumer'));
-//		Yii::app()->end();
-		Yii::app()->api->oauthInit($this->callbackUrl);
+		$this->api->oauthInit($this->callbackUrl);
 	}
 	
 	public function actionCallback() {
-		Yii::app()->api->oauthCallback($this->callbackUrl);
+		$this->api->oauthCallback($this->callbackUrl);
 	}
 	
 	public function actionDelete() {
 		$this->api->instance->delete();
 	}
 	
+	private $_api = null;
 	public function getApi() {
-		return Yii::app()->api;
+		if($this->_api === null) {
+			$this->_api = $this->getApiObject();
+		}
+		return $this->_api;
+	}
+	
+	protected function getApiObject() {
+		return Yii::app()->tpapi;
 	}
 }
--- TPAPI/user/controllers/AppController.php
+++ TPAPI/user/controllers/AppController.php
@@ -2,6 +2,13 @@
 
 class AppController extends CTPAppController
 {
+	/*
+	protected function getApiObject() {
+		// Modify this if you use an API component identifier other than tpapi
+		return Yii::app()->tpapi;
+	}
+	//*/
+	
 	public function actionIndex() {
 		
 	}
